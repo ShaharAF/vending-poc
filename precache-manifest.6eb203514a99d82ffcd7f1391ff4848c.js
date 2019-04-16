dd3e6a3d0ce43739dcc2c27d069c942f0f5f9a8b
shaharc1990 <shaharc1990@gmail.com>
2019-04-16 11:39:17 +0300

    Updates

---
+++ precache-manifest.6eb203514a99d82ffcd7f1391ff4848c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7eae0d3bb7e94d47db5c18100c98d64",
+    "url": "/vending-poc/static/media/AppsFlyer.f7eae0d3.png"
+  },
+  {
+    "revision": "0b3de46d4f75d762715a",
+    "url": "/vending-poc/static/js/runtime~main.7390d84f.js"
+  },
+  {
+    "revision": "e8dca094beb4314d228d",
+    "url": "/vending-poc/static/js/main.b5acd9b2.chunk.js"
+  },
+  {
+    "revision": "e99b4541a6e3c4fd8169",
+    "url": "/vending-poc/static/js/2.00c9e843.chunk.js"
+  },
+  {
+    "revision": "7e451e8b98d1609e4c8a03f031846ff7",
+    "url": "/vending-poc/index.html"
+  }
+];
